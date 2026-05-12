158468345487a5cdb904a51ace060d1dc47c7c20
Jerome Aguas <90044654+jeyinked@users.noreply.github.com>
2026-05-12 22:25:36 +0200

    Add basic HTTP server in app.py

---
+++ docker-compose-scaling-lab/api/app.py
@@ -0,0 +1,15 @@
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOSTNAME = socket.gethostname()
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = f"Hello from API container: {HOSTNAME}\n"
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+server = HTTPServer(("0.0.0.0", 5000), Handler)
+server.serve_forever()
